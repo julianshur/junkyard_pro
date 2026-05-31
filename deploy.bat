fb7bc6f6033ee674995a0233ba334be02b1bb0c1
Julian S <julianshur@gmail.com>
2026-05-31 00:35:40 -0700

    Auto deploy

---
+++ deploy.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal
+
+set FTP_HOST=ftp.junkyardpro.com
+set FTP_USER=julianshur@junkyardpro.com
+set REMOTE_DIR=/
+
+echo Enter FTP password:
+set /p FTP_PASS=b1n6b0n61!
+
+echo.
+echo === Git add ===
+git add .
+
+echo === Git commit ===
+set /p MSG="Commit message (or press enter): "
+if "%MSG%"=="" set MSG=Auto deploy
+git commit -m "%MSG%"
+git push
+
+echo.
+echo === Creating WinSCP script ===
+
+(
+echo open ftps://%FTP_USER%:%FTP_PASS%@%FTP_HOST%/ -explicit
+echo option batch abort
+echo option confirm off
+echo cd %REMOTE_DIR%
+echo synchronize remote . .
+echo exit
+) > winscp_script.txt
+
+echo === Uploading to Turbify ===
+"C:\Program Files (x86)\WinSCP\WinSCP.com" /script=winscp_script.txt
+
+del winscp_script.txt
+
+echo.
+echo === DONE ===
+pause
