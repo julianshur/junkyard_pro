5f900a6783157b12c10e6f2643b758b6da4a1815
Julian S <julianshur@gmail.com>
2026-05-31 20:30:30 -0700

    asd

--- deploy.bat
+++ deploy.bat
@@ -1,39 +1,10 @@
-@echo off
-setlocal
-
-set "REPO_DIR=C:\Users\Julian\Documents\git\junkyard_pro"
-
-echo ==========================
-echo   TARGETED DEPLOY
-echo ==========================
-
-echo.
-echo === Git commit/push ===
-cd /d "%REPO_DIR%"
-
-git add server.js public/index.html package.json
-
-git diff --cached --quiet
-if errorlevel 1 (
-    git commit -m "Auto deploy (targeted files)"
-)
-
-git push
-
-echo.
-echo === Deploy via WinSCP ===
-
 "C:\Program Files (x86)\WinSCP\WinSCP.com" ^
  /log=deploy.log ^
  /command ^
  "open junkyard_ftp" ^
  "cd /" ^
- "lcd C:\Users\Julian\Documents\git\junkyard_pro" ^
- "put server.js /server.js" ^
- "put package.json /package.json" ^
- "put public/index.html public/index.html" ^
+ "put ""%REPO%\server.js"" /server.js" ^
+ "put ""%REPO%\package.json"" /package.json" ^
+ "put ""%REPO%\public\index.html"" /index.html" ^
+ "synchronize remote -filemask=""|.git/;node_modules/;deploy.log;*.md;*.env"" -delete" ^
  "exit"
-
-echo.
-echo === DONE ===
-pause
