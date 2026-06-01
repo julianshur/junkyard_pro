92777efb1f3f45b1496744c0888db371052d7dea
Julian S <julianshur@gmail.com>
2026-05-31 21:04:25 -0700

    deploy

--- deploy.bat
+++ deploy.bat
@@ -1,12 +1,37 @@
+@echo off
+setlocal
+
+set "REPO=C:\Users\Julian\Documents\git\junkyard_pro"
+
+echo ==========================
+echo   ZERO-BUG DEPLOY
+echo ==========================
+
+cd /d "%REPO%" || exit /b 1
+
+echo.
+echo === Git push ===
+git add -A
+git commit -m "deploy" >nul 2>&1
+git push
+
+echo.
+echo === WinSCP deploy ===
+
 "C:\Program Files (x86)\WinSCP\WinSCP.com" ^
  /log=deploy.log ^
- /loglevel=2 ^
+ /loglevel=1 ^
  /command ^
  "open junkyard_ftp" ^
- "option confirm off" ^
  "option batch on" ^
+ "option confirm off" ^
+ "lcd %REPO%" ^
  "cd /" ^
- "put ""%REPO%\server.js"" /server.js" ^
- "put ""%REPO%\package.json"" /package.json" ^
- "put ""%REPO%\public\index.html"" /index.html" ^
+ "put server.js /server.js" ^
+ "put package.json /package.json" ^
+ "put public/index.html /index.html" ^
  "exit"
+
+echo.
+echo === DONE ===
+pause
